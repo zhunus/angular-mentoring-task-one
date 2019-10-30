ce31516923a3df60e9533c928de142473fff1e34
Zhunis Gaziz <zhunis_gaziz@epam.com>
2019-10-30 09:46:26 +0600

    Time pipe tests

--- src/app/shared/pipes/time.pipe.spec.ts
+++ src/app/shared/pipes/time.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { TimePipe } from './time.pipe';
 
 describe('TimePipe', () => {
+  const pipe = new TimePipe();
+
   it('create an instance', () => {
-    const pipe = new TimePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform number to formatted time', () => {
+    expect(pipe.transform(60 * 1000)).toEqual('1 minute');
+  });
+
+  it('should transform 5000 to 5 seconds', () => {
+    expect(pipe.transform(5 * 1000)).toEqual('5 seconds');
   });
 });
