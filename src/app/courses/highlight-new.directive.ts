6760dc475307c6aa8599707d4a73b09c804ca318
Zhunis Gaziz <zhunis_gaziz@epam.com>
2019-11-11 11:29:59 +0600

    Fixed updating directive

--- src/app/courses/highlight-new.directive.ts
+++ src/app/courses/highlight-new.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 import { differenceInDays } from 'date-fns';
 
 @Directive({
   selector: '[appHighlightNew]'
 })
-export class HighlightNewDirective implements OnInit {
+export class HighlightNewDirective implements OnChanges {
   @Input('appHighlightNew')
   createdDate: Date;
 
@@ -15,7 +15,7 @@ export class HighlightNewDirective implements OnInit {
     this.el.nativeElement.style.border = `2px solid ${color}`;
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     const currentDate = new Date();
     const diff = differenceInDays(currentDate, this.createdDate);
     if (this.createdDate < currentDate && diff < 14) {
