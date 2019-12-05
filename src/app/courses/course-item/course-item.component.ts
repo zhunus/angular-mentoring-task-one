885f9405ce44c6ad79ae0433a4796325d12d4067
Zhunis Gaziz <zhunis_gaziz@epam.com>
2019-12-05 14:36:54 +0600

    Changed change detection strategy

--- src/app/courses/course-item/course-item.component.ts
+++ src/app/courses/course-item/course-item.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from '../../entities/course.entity';
 
 @Component({
   selector: 'app-course-item',
   templateUrl: './course-item.component.html',
-  styleUrls: ['./course-item.component.css']
+  styleUrls: ['./course-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseItemComponent implements OnInit {
   @Input()
--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from '../../entities/course.entity';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.css']
+  styleUrls: ['./courses-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CoursesListComponent implements OnInit {
   @Input()
